633998fc020d970ddc0197b2f3f07e38a42cd554
Jonatas Silva <54430451+jonatasosilva@users.noreply.github.com>
2020-12-26 10:14:22 -0300

    finish up functions

---
+++ socialape-functions/functions/utils/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyBnnyfKy8nKU0cMhR3r983LM7XUaW7M-2o",
+  authDomain: "socialape-2af26.firebaseapp.com",
+  projectId: "socialape-2af26",
+  storageBucket: "socialape-2af26.appspot.com",
+  messagingSenderId: "1064819634239",
+  appId: "1:1064819634239:web:62e0efd9af4c15c70b888a",
+  measurementId: "G-5Q5DZ7E7NE",
+};
